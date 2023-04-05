22da57abfc4f02dbd667976d4f07eb783b1b2b2d
ReeTeemo <126101788+ReeTeemo@users.noreply.github.com>
2023-04-05 16:27:53 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+let homeScoreEl = document.getElementById("home-score")
+let homeScore = 0
+
+let homeScoreBtnOne = document.getElementById("home-score-btn-1")
+
+function increaseHomeScoreOne(){
+    homeScore += 1
+    homeScoreEl.textContent = homeScore
+}
+
+let homeScoreBtnTwo = document.getElementById("home-score-btn-2")
+
+function decreaseHomeScoreOne(){
+    homeScore -= 1
+    homeScoreEl.textContent = homeScore
+}
+
+let awayScoreEl = document.getElementById("away-score")
+let awayScore = 0
+
+let awayScoreBtnOne = document.getElementById("away-score-btn-1")
+
+function increaseAwayScoreOne(){
+    awayScore += 1
+    awayScoreEl.textContent = awayScore
+}
+
+let awayScoreBtnTwo = document.getElementById("away-score-btn-2")
+
+function decreaseAwayScoreOne(){
+    awayScore -= 1
+    awayScoreEl.textContent = awayScore
+}
+
+document.getElementById("resetBtn").onclick = 
+
+function(){
+    count=0
+    document.getElementById("home-score").innerHTML = count
+}
